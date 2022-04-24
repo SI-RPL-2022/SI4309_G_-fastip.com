86d7ef534f26754f493427ab347b8b6da12f9b37
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-04-24 19:21:13 +0700

    About #3

---
+++ TUBES/resources/views/pmi/aboutDonor.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('content')
+<div class="container py-5">
+
+    <p>Salah satu kegiatan PMI yang paling dikenal masyarakat adalah donor darah. Menyumbangkan sebagian darah untuk
+        kemudian disalurkan kepada yang membutuhkan menjadi suatu sumbangan berarti dalam kehidupan sosial
+        bermasyarakat. Tidak membutuhkan persyaratan sulit untuk menjadi calon donor.</p>
+    <b>Syarat Untuk Menjadi Donor Darah :</b>
+    <p>Donor darah adalah orang yang memberikan darah secara sukarela untuk maksud dan tujuan transfusi darah bagi orang
+        lain yang membutuhkan. Semua orang dapat menjadi donor darah jika memenuhi persyaratan yang berlaku.</p>
+    <b>APA SYARAT SYARAT UNTUK MENJADI DONOR DARAH ?</b>
+    <ol>
+       
