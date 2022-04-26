b4fe49adcde41a54e6c7b58a6b53b1a27a5571f1
Sahrabifa <sabifa0251@gmail.com>
2022-04-27 06:50:11 +0700

    route

--- TUBES/routes/web.php
+++ TUBES/routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function () {
+    return view('index');
+})->name('dashboard');
 
-Route::post('/profile', [\App\Http\Controllers\AuthController::class, 'updateProfile'])->name('profile.update');
+Route::get('/registration', [\App\Http\Controllers\AuthController::class, 'registration'])->name('registration');
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
+
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'index'])->name('login');
+Route::post('/login-process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login.process');
 
