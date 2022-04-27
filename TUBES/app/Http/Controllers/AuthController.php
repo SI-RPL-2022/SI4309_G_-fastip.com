fc5548959fa90ddf3f19aedba37c2562ceaafd12
Hosxen <Fadhillahfatih@gmail.com>
2022-04-27 21:48:12 +0700

    new update

--- TUBES/app/Http/Controllers/AuthController.php
+++ TUBES/app/Http/Controllers/AuthController.php
@@ -67,7 +67,6 @@ class AuthController extends Controller
     public function me()
     {
         return view('auth.profile');
-<<<<<<< HEAD
     }
 
     public function updateProfile(Request $request)
@@ -89,6 +88,3 @@ class AuthController extends Controller
         return redirect('me')->with('success', 'Berhasil mengubah profile!');
     }
 }
-=======
-    }
->>>>>>> 0bc5aafe953770afe6b47188a3dddb9345e7fc05
