ab33b998d4a80eb83298256fe25c0609faa6c352
PutriNelsa <putrinelsa@student.telkomuniversity.ac.id>
2022-04-27 22:16:59 +0700

    dasboard navbar #2

--- TUBES/resources/views/index.blade.php
+++ TUBES/resources/views/index.blade.php
@@ -61,4 +61,40 @@
                                             <option>O</option>
                                         </select>
                                     </div>
-                                
+                                </div>
+                                <div class="card-body py-0">
+                                    <div class="form-group text-left">
+                                        <label for="component">Pilih Komponen</label>
+                                        <select class="form-control" id="component">
+                                            <option>-Pilih Komponen-</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="card-body py-0">
+                                    <div class="form-group text-left">
+                                        <label for="province">Pilih Provinsi</label>
+                                        <select class="form-control" id="province">
+                                            <option>-Pilih Provinsi-</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <a href="{{ route('blood-stock') }}" type="button" class="btn btn-danger">CARI</a>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 mt-5 text-center">
+                        <div class="card h-100">
+                            <div class="card-header bg-red100 text-light text-left">
+                                <b> Kegiatan Donor </b> | 17 - 1 - 2022
+                            </div>
+                            <div class="card-body">
+                                <ol class="text-left">
+                                    <li><a href="">Kegiatan di Provinsi Bali</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Banten</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Jawa Barat</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Jawa Tengah</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Jawa Timur</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Lampung</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Sulawesi Barat</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Sumatera Selatan</a></li>
+                                    <li><a href="">Kegiatan di Provinsi Sumatera Utara</a></li>
