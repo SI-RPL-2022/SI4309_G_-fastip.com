a0f8089435afa87d2c282546ba85d90fb7d1906e
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-06-14 20:00:18 +0700

    Update #11

---
+++ TUBES/resources/views/pmi/bloodStock.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+@section('content')
+<div class="container pb-5">
+    <div class="row">
+        <div class="col-md-4 col-sm-12 mt-5 text-center">
+            <div class="card h-100">
+                <div class="card-header bg-red100 text-light">
+                    CARI STOK DARAH
+                </div>
+                <form action="" class="overflow-auto p-3">
+                    <div class="card-body py-0 pt-3">
+                        <div class="form-group text-left">
+                            <label for="bloodType">Golongan Darah</label>
+                            <select class="form-control" id="bloodType">
+                                <option>A+</option>
+                                <option>AB</option>
+                                <option>B</option>
+                                <option>O</option>
+                            </select>
+                        </div>
