a914af8b57939b371f6ebc807ec4b5e24d5ba759
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-06-15 20:29:08 +0700

    Update again #11

--- TUBES/resources/views/pmi/bloodStock.blade.php
+++ TUBES/resources/views/pmi/bloodStock.blade.php
@@ -39,3 +39,20 @@
                 </form>
             </div>
         </div>
+        <div class="col-md-4 col-sm-12 mt-5 text-center">
+            <div class="card h-100">
+                <div class="card-header bg-red100 text-light">
+                    CARI TEMAN
+                </div>
+                <form action="" class="overflow-auto p-3">
+                    <div class="card-body py-0 pt-3">
+                        <div class="form-group text-left">
+                            <label for="bloodType">Golongan Darah</label>
+                            <select class="form-control" id="bloodType">
+                                <option>A+</option>
+                                <option>AB</option>
+                                <option>B</option>
+                                <option>O</option>
+                            </select>
+                        </div>
+                    </div>
