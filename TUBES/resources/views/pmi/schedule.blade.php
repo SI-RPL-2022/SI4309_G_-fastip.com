9a176bc9fd17f23a8366a49b1fcbc77621a80d9b
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-06-15 20:50:07 +0700

    Update part Schedule #11

---
+++ TUBES/resources/views/pmi/schedule.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('content')
+    <div class="container pb-5">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 mt-5 text-center">
+                <div class="card h-100">
+                    <div class="card-header bg-red100 text-light">
+                        KEGIATAN DONOR DARAH
+                    </div>
