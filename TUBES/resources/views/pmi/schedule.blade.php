91c3559c5f7e4ea8981374b9a72d75eafb8f8ead
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-06-17 18:27:37 +0700

    Update part schedule #11

--- TUBES/resources/views/pmi/schedule.blade.php
+++ TUBES/resources/views/pmi/schedule.blade.php
@@ -7,3 +7,18 @@
                     <div class="card-header bg-red100 text-light">
                         KEGIATAN DONOR DARAH
                     </div>
+                    <form action="" class="overflow-auto p-3">
+                        <div class="card-body py-0 pt-3">
+                            <div class="form-group text-left">
+                                <label>Tanggal Donor</label>
+                                <input type="date" class="form-control" id="date" placeholder="">
+                            </div>
+                        </div>
+                        <div class="card-body py-0">
+                            <div class="form-group text-left">
+                                <label for="province">Pilih Provinsi</label>
+                                <select class="form-control" id="province">
+                                    <option>-Pilih Provinsi-</option>
+                                </select>
+                            </div>
+                        </div>
