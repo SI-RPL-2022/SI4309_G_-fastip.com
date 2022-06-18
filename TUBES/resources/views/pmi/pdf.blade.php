202e3717c078a1dee4c6de24b92a106b1048345c
FarhanaZahra <farhanazahra@student.telkomuniversity.ac.id>
2022-06-18 22:36:10 +0700

    Update PDF #15

--- TUBES/resources/views/pmi/pdf.blade.php
+++ TUBES/resources/views/pmi/pdf.blade.php
@@ -6,3 +6,35 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        .bg-red {
+            background-color: red;
+        }
+
+        .bg-white {
+            background-color: white;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .center {
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .text-left {
+            text-align: left;
+        }
+
+        h5 {
+            font-size: 20px;
+        }
+
+        .m-0 {
+            margin: 0;
+        }
+    </style>
+</head>
