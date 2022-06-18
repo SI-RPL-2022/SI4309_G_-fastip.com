6bb53bc3e7997d1aa51ed6172fe98a55ecedbb64
Sahrabifa <sabifa0251@gmail.com>
2022-06-18 21:57:19 +0700

    fix bug #14

--- TUBES/resources/views/pmi/findFriend.blade.php
+++ TUBES/resources/views/pmi/findFriend.blade.php
@@ -27,10 +27,10 @@
                                 <td>{{ $user['blood_type'] }}</td>
                                 @if(auth()->check() and auth()->user()->isAdmin())
                                     <td>
-                                        <form method="post" action="{{ url("user/{$user['id']}") }}">
+                                        <form method="post" action="{{url("user/{$user['id']}") }}">
                                             @csrf
                                             @method('delete')
-                                            <a href="{{ url("user/{$user['id']}/edit") }}" type="button" class="btn btn-success m-2">Update</a>
+                                            <a href="{{url("user/{$user['id']}/edit") }}" type="button" class="btn btn-success m-2">Update</a>
                                             <button type="submit" class="btn btn-danger m-2">Delete</button>
                                         </form>
                                     </td>
