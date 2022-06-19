8279fb25cb82ee9797060ef124f0e3b206a8786e
Sahrabifa <sabifa0251@gmail.com>
2022-06-20 05:08:47 +0700

    update findfriend admin

---
+++ TUBES/resources/views/pmi/findFriendAdmin.blade.php
@@ -0,0 +1,31 @@
+<?php include "views/layout/navbar.php" ?>
+
+<div class="container pb-5">
+    <div class="row my-5 mx-1">
+        <div class="col-12 bg-red10 table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Nomor Handphone</th>
+                    <th scope="col">Golongan Darah</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Bandung</td>
+                    <td>081776553445</td>
+                    <td>A</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+
+<?php include "views/layout/footer.php" ?>
