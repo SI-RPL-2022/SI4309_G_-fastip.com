abb3f66091bf1d0eb5acb6b0bfc0ed32c788a825
Sahrabifa <sabifa0251@gmail.com>
2022-07-02 06:00:55 +0700

    revisi

--- TUBES/database/migrations/2014_10_12_000000_create_users_table.php
+++ TUBES/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->enum('blood_type', ['A', 'B', 'AB', 'O'])->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,4 +37,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
